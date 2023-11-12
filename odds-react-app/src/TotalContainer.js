9f7eed6f5f90ed42310cc80bf0583ab829187565
seoular <34080384+seoular@users.noreply.github.com>
2023-11-12 03:49:54 +0200

    Update TotalContainer.js

--- odds-react-app/src/TotalContainer.js
+++ odds-react-app/src/TotalContainer.js
@@ -3,190 +3,199 @@ import React, { useState, useEffect } from "react";
 import { PlayerPosMap } from "./constants";
 import SangTable from "./SangTable";
 
+
 function TotalContainer() {
-  const [selectedPosition, setSelectedPosition] = useState(0);
-  const [playerList, setPlayerList] = useState([]);
+ const [selectedPosition, setSelectedPosition] = useState(0);
+ const [playerList, setPlayerList] = useState([]);
 
-  const scrapeData = async (pos) => {
-    const sangPProps = new Map();
-    var getUrl =
-      "https://raw.githubusercontent.com/seoular/test/main/footballodds";
-    await fetch(getUrl)
-      .then((response) => {
-        return response;
-      })
-      .then((data) => {
-        let allNflGames = data[0].events.slice();
 
-        for (let i = 0; i < allNflGames.length; i++) {
-          let eachGameTDOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-1870")
-            ?.markets.find(
-              (y) =>
-                y.descriptionKey == "Anytime Touchdown Scorer" &&
-                y.period.id == "119"
-            )?.outcomes;
-          let eachGameRushingOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-93")
-            ?.markets.filter((y) => y.marketTypeId == "121337");
-          let eachGameReceivingOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-94")
-            ?.markets.filter((y) => y.marketTypeId == "121333");
-          let eachGameReceptionOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-94")
-            ?.markets.filter((y) => y.marketTypeId == "121332");
-          let eachGamePassingYdOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-1188")
-            ?.markets.filter((y) => y.marketTypeId == "121348");
-          let eachGamePassingTDOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-1188")
-            ?.markets.filter((y) => y.marketTypeId == "121335");
-          let eachGameIntOutcomes = allNflGames[i].displayGroups
-            .find((x) => x.id == "100-1188")
-            ?.markets.filter((y) => y.marketTypeId == "121329");
-          if (typeof eachGameTDOutcomes !== "undefined") {
-            for (let j = 0; j < eachGameTDOutcomes.length; j++) {
-              let playerOdds = eachGameTDOutcomes[j];
-              // get formula right later
-              sangPProps.set(
-                playerOdds.description,
-                (1 / playerOdds.price.decimal) * 6
-              );
-            }
-          }
-          if (typeof eachGameRushingOutcomes !== "undefined") {
-            for (let j = 0; j < eachGameRushingOutcomes.length; j++) {
-              let playerOdds = eachGameRushingOutcomes[j];
-              let name = playerOdds.description.slice(22);
-              let temp = sangPProps.get(name);
-              if (typeof temp == "undefined") {
-                temp = 0;
-              }
-              sangPProps.set(
-                name,
-                temp + playerOdds.outcomes[0].price.handicap / 10
-              );
-            }
-          }
-          if (typeof eachGameReceivingOutcomes !== "undefined") {
-            for (let j = 0; j < eachGameReceivingOutcomes.length; j++) {
-              let playerOdds = eachGameReceivingOutcomes[j];
-              let name = playerOdds.description.slice(24);
-              let temp = sangPProps.get(name);
-              if (typeof temp == "undefined") {
-                temp = 0;
-              }
-              sangPProps.set(
-                name,
-                temp + playerOdds.outcomes[0].price.handicap / 10
-              );
-            }
-          }
-          if (typeof eachGameReceptionOutcomes !== "undefined") {
-            for (let j = 0; j < eachGameReceptionOutcomes.length; j++) {
-              let playerOdds = eachGameReceptionOutcomes[j];
-              let name = playerOdds.description.slice(19);
-              let temp = sangPProps.get(name);
-              if (typeof temp == "undefined") {
-                temp = 0;
-              }
-              sangPProps.set(
-                name,
-                temp + playerOdds.outcomes[0].price.handicap * 0.5
-              );
-            }
-          }
-          if (typeof eachGamePassingYdOutcomes !== "undefined") {
-            for (let j = 0; j < eachGamePassingYdOutcomes.length; j++) {
-              let playerOdds = eachGamePassingYdOutcomes[j];
-              let name = playerOdds.description.slice(22);
-              let temp = sangPProps.get(name);
-              if (typeof temp == "undefined") {
-                temp = 0;
-              }
-              sangPProps.set(
-                name,
-                temp + playerOdds.outcomes[0].price.handicap / 25
-              );
-            }
-          }
-          if (typeof eachGamePassingTDOutcomes !== "undefined") {
-            for (let j = 0; j < eachGamePassingTDOutcomes.length; j++) {
-              let playerOdds = eachGamePassingTDOutcomes[j];
-              let name = playerOdds.description.slice(27);
-              let temp = sangPProps.get(name);
-              if (typeof temp == "undefined") {
-                temp = 0;
-              }
-              sangPProps.set(
-                name,
-                temp + playerOdds.outcomes[0].price.handicap * 4
-              );
-            }
-          }
-          if (typeof eachGameIntOutcomes !== "undefined") {
-            for (let j = 0; j < eachGameIntOutcomes.length; j++) {
-              let playerOdds = eachGameIntOutcomes[j];
-              let name = playerOdds.description.slice(29);
-              let temp = sangPProps.get(name);
-              if (typeof temp == "undefined") {
-                temp = 0;
-              }
-              sangPProps.set(
-                name,
-                temp + playerOdds.outcomes[0].price.handicap * -2
-              );
-            }
-          }
-        }
+ const scrapeData = async (pos) => {
+   const sangPProps = new Map();
+   var getUrl =
+     "https://raw.githubusercontent.com/seoular/test/main/footballodds111123";
+   await fetch(getUrl)
+     .then((response) => {
+       return response.json();
+     })
+     .then((data) => {
+       let allNflGames = data[0].events.slice();
 
-        const mapEntries = Array.from(sangPProps.entries());
-        // Sort the array based on the numeric value (assuming values are numbers)
-        mapEntries.sort((a, b) => b[1] - a[1]);
-        // Create a new Map from the sorted array
-        const sortedMap = new Map(mapEntries);
-        let finalList = Array.from(sortedMap.entries()).filter(
-          (x) =>
-            typeof PlayerPosMap.get(x[0]) !== "undefined" &&
-            PlayerPosMap.get(x[0]) == pos &&
-            x[1] > 5
-        );
+       for (let i = 0; i < allNflGames.length; i++) {
+         let eachGameTDOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-1870")
+           ?.markets.find(
+             (y) =>
+               y.descriptionKey == "Anytime Touchdown Scorer" &&
+               y.period.id == "119"
+           )?.outcomes;
+         let eachGameRushingOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-93")
+           ?.markets.filter((y) => y.marketTypeId == "121337");
+         let eachGameReceivingOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-94")
+           ?.markets.filter((y) => y.marketTypeId == "121333");
+         let eachGameReceptionOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-94")
+           ?.markets.filter((y) => y.marketTypeId == "121332");
+         let eachGamePassingYdOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-1188")
+           ?.markets.filter((y) => y.marketTypeId == "121348");
+         let eachGamePassingTDOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-1188")
+           ?.markets.filter((y) => y.marketTypeId == "121335");
+         let eachGameIntOutcomes = allNflGames[i].displayGroups
+           .find((x) => x.id == "100-1188")
+           ?.markets.filter((y) => y.marketTypeId == "121329");
+         if (typeof eachGameTDOutcomes !== "undefined") {
+           for (let j = 0; j < eachGameTDOutcomes.length; j++) {
+             let playerOdds = eachGameTDOutcomes[j];
+             // get formula right later
+             sangPProps.set(
+               playerOdds.description,
+               (1 / playerOdds.price.decimal) * 6
+             );
+           }
+         }
+         if (typeof eachGameRushingOutcomes !== "undefined") {
+           for (let j = 0; j < eachGameRushingOutcomes.length; j++) {
+             let playerOdds = eachGameRushingOutcomes[j];
+             let name = playerOdds.description.slice(22);
+             let temp = sangPProps.get(name);
+             if (typeof temp == "undefined") {
+               temp = 0;
+             }
+             sangPProps.set(
+               name,
+               temp + playerOdds.outcomes[0].price.handicap / 10
+             );
+           }
+         }
+         if (typeof eachGameReceivingOutcomes !== "undefined") {
+           for (let j = 0; j < eachGameReceivingOutcomes.length; j++) {
+             let playerOdds = eachGameReceivingOutcomes[j];
+             let name = playerOdds.description.slice(24);
+             let temp = sangPProps.get(name);
+             if (typeof temp == "undefined") {
+               temp = 0;
+             }
+             sangPProps.set(
+               name,
+               temp + playerOdds.outcomes[0].price.handicap / 10
+             );
+           }
+         }
+         if (typeof eachGameReceptionOutcomes !== "undefined") {
+           for (let j = 0; j < eachGameReceptionOutcomes.length; j++) {
+             let playerOdds = eachGameReceptionOutcomes[j];
+             let name = playerOdds.description.slice(19);
+             let temp = sangPProps.get(name);
+             if (typeof temp == "undefined") {
+               temp = 0;
+             }
+             sangPProps.set(
+               name,
+               temp + playerOdds.outcomes[0].price.handicap * 0.5
+             );
+           }
+         }
+         if (typeof eachGamePassingYdOutcomes !== "undefined") {
+           for (let j = 0; j < eachGamePassingYdOutcomes.length; j++) {
+             let playerOdds = eachGamePassingYdOutcomes[j];
+             let name = playerOdds.description.slice(22);
+             let temp = sangPProps.get(name);
+             if (typeof temp == "undefined") {
+               temp = 0;
+             }
+             sangPProps.set(
+               name,
+               temp + playerOdds.outcomes[0].price.handicap / 25
+             );
+           }
+         }
+         if (typeof eachGamePassingTDOutcomes !== "undefined") {
+           for (let j = 0; j < eachGamePassingTDOutcomes.length; j++) {
+             let playerOdds = eachGamePassingTDOutcomes[j];
+             let name = playerOdds.description.slice(27);
+             let temp = sangPProps.get(name);
+             if (typeof temp == "undefined") {
+               temp = 0;
+             }
+             sangPProps.set(
+               name,
+               temp + playerOdds.outcomes[0].price.handicap * 4
+             );
+           }
+         }
+         if (typeof eachGameIntOutcomes !== "undefined") {
+           for (let j = 0; j < eachGameIntOutcomes.length; j++) {
+             let playerOdds = eachGameIntOutcomes[j];
+             let name = playerOdds.description.slice(29);
+             let temp = sangPProps.get(name);
+             if (typeof temp == "undefined") {
+               temp = 0;
+             }
+             sangPProps.set(
+               name,
+               temp + playerOdds.outcomes[0].price.handicap * -2
+             );
+           }
+         }
+       }
 
-        setPlayerList(finalList);
 
-        return finalList;
-      });
-    // .catch((err) => {
-    //   return [];
-    //   // Do something for an error here
-    // });
-  };
+       const mapEntries = Array.from(sangPProps.entries());
+       // Sort the array based on the numeric value (assuming values are numbers)
+       mapEntries.sort((a, b) => b[1] - a[1]);
+       // Create a new Map from the sorted array
+       const sortedMap = new Map(mapEntries);
+       let finalList = Array.from(sortedMap.entries()).filter(
+         (x) =>
+           typeof PlayerPosMap.get(x[0]) !== "undefined" &&
+           PlayerPosMap.get(x[0]) == pos &&
+           x[1] > 5
+       );
 
-  useEffect(() => {
-    scrapeData(selectedPosition).catch(console.error);
-  }, [selectedPosition]);
 
-  return (
-    <div>
-      <div>
-        <div>
-          <select
-            defaultValue={selectedPosition}
-            onChange={(e) => {
-              setSelectedPosition(parseInt(e.target.value));
-            }}
-            style={{ display: "flex", marginLeft: "20px" }}
-          >
-            <option value="0">QB</option>
-            <option value="1">RB</option>
-            <option value="2">WR</option>
-            <option value="3">TE</option>
-          </select>
-        </div>
-        <SangTable selectedPosition={selectedPosition} evList={playerList} />
-      </div>
-    </div>
-  );
+       setPlayerList(finalList);
+
+
+       return finalList;
+     });
+   // .catch((err) => {
+   //   return [];
+   //   // Do something for an error here
+   // });
+ };
+
+
+ useEffect(() => {
+   scrapeData(selectedPosition).catch(console.error);
+ }, [selectedPosition]);
+
+
+ return (
+   <div>
+     <div>
+       <div>
+         <select
+           defaultValue={selectedPosition}
+           onChange={(e) => {
+             setSelectedPosition(parseInt(e.target.value));
+           }}
+           style={{display: 'flex', marginLeft: '20px'}}
+         >
+           <option value="0">QB</option>
+           <option value="1">RB</option>
+           <option value="2">WR</option>
+           <option value="3">TE</option>
+         </select>
+       </div>
+       <SangTable selectedPosition={selectedPosition} evList={playerList} />
+      
+     </div>
+   </div>
+ );
 }
+
 
 export default TotalContainer;
