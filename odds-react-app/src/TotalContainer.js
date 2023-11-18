6640ac25f56dc6698b6c7ac152eb80f615566bc6
seoular <34080384+seoular@users.noreply.github.com>
2023-11-19 00:04:29 +0200

    Update TotalContainer.js

--- odds-react-app/src/TotalContainer.js
+++ odds-react-app/src/TotalContainer.js
@@ -404,7 +404,7 @@ function TotalContainer() {
       
       </div>
       <div class="updateTimeSection" >
-        EV values last updated Friday, 11/17 at 5:59pm ET
+        EV values last updated Saturday, 11/18 at 4:00pm ET
       </div>
       <div class="patreonSection">
         <div>
