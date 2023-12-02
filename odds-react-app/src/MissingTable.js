cc6a19f6021575ce2b907062f9c80f0587f9e514
seoular <34080384+seoular@users.noreply.github.com>
2023-12-02 13:01:18 -0800

    remove missingplayers message from flex and superflex positions page

--- odds-react-app/src/MissingTable.js
+++ odds-react-app/src/MissingTable.js
@@ -1,28 +1,32 @@
 import "./styles.css";
 import React, { useState, useEffect } from "react";
 
-
-
 export default function MissingTable(props) {
   return (
     <div className="MissingTable">
-      <table style={{marginTop: '20px', fontSize:'12px'}}>
+      <table style={{ marginTop: "20px", fontSize: "12px" }}>
         <tr>
-          <th
-            style={{
-              width: "300px"
-            }}
-          >
-            Players missing all required props:
-          </th>
+          {props.selectedPosition !== 99 && props.selectedPosition !== 98 ? (
+            <th
+              style={{
+                width: "300px",
+              }}
+            >
+              Players missing all required props:
+            </th>
+          ) : (
+            <th
+              style={{
+                width: "300px",
+              }}
+            ></th>
+          )}
         </tr>
-        {props.missingList.map((player) => 
+        {props.missingList.map((player) => (
           <tr>
-            <td>
-                {player} 
-            </td>
-          </tr>        
-        )}
+            <td>{player}</td>
+          </tr>
+        ))}
       </table>
     </div>
   );
