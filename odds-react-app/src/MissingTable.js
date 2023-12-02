02ce2d20b0a854a62f60081026554c7ad137eaaa
seoular <34080384+seoular@users.noreply.github.com>
2023-12-02 13:31:47 -0800

    update missingtable to new layout and accept new value pattern

--- odds-react-app/src/MissingTable.js
+++ odds-react-app/src/MissingTable.js
@@ -4,16 +4,32 @@ import React, { useState, useEffect } from "react";
 export default function MissingTable(props) {
   return (
     <div className="MissingTable">
-      <table style={{ marginTop: "20px", fontSize: "12px" }}>
+      <table style={{ marginTop: "20px", fontSize: "12px", textAlign: "left" }}>
         <tr>
           {props.selectedPosition !== 99 && props.selectedPosition !== 98 ? (
-            <th
-              style={{
-                width: "300px",
-              }}
-            >
-              Players missing all required props:
-            </th>
+            <>
+              <th
+                style={{
+                  width: "100px",
+                }}
+              >
+                Player name
+              </th>
+              <th
+                style={{
+                  width: "150px",
+                }}
+              >
+                Missing prop(s)
+              </th>
+              <th
+                style={{
+                  width: "50px",
+                }}
+              >
+                EV without prop(s)
+              </th>
+            </>
           ) : (
             <th
               style={{
@@ -24,7 +40,9 @@ export default function MissingTable(props) {
         </tr>
         {props.missingList.map((player) => (
           <tr>
-            <td>{player}</td>
+            <td>{player[0]}</td>
+            <td>{player[1]}</td>
+            <td>{player[2]}</td>
           </tr>
         ))}
       </table>
