60fd81689398ede53d6308e69d576b28276c5b56
seoular <34080384+seoular@users.noreply.github.com>
2023-12-16 14:39:07 -0800

    add joe flacco

--- odds-react-app/src/constants.js
+++ odds-react-app/src/constants.js
@@ -31,6 +31,7 @@ export const slotcodes = {
   // Add more mappings as needed based on your specific data structure
 };
 export const PlayerPosMap = new Map([
+  ["Joe Flacco", Positions.QB],
   ["Jake Browning", Positions.QB],
   ["Desmond Ridder", Positions.QB],
   ["Isaiah Likely", Positions.TE],
